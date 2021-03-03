29359796c28dcfc06a7bcbb4ffad054803dfc8a4
Dima <webbootcamp2020@gmail.com>
2021-03-03 23:29:59 +0200

    Added hero slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import '../sass/style.scss';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 
 import NavigationMenu from './navigationMenu.js';
 
 const navMenu = new NavigationMenu();
+
+const swiper = new Swiper('.swiper-container',{
+	loop: true,
+	grabCursor: true,
+});
